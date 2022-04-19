d92ee16461f25f32c751dd7e5519a28871ab5782
Phillip48 <phillippereira02@gmail.com>
2022-04-19 16:55:58 -0400

    Appened data from api onto page

--- assests/js/country.js
+++ assests/js/country.js
@@ -2,7 +2,7 @@ var countryInfo = [];
 
 var submitBtn = document.getElementById("Submit-btn");
 var inputArea = document.getElementById("Search-box");
-let countryFlagEl = document.getElementById('country-flag')
+let countryImg = document.getElementById('country-img')
 
 // Function to fetch data from Country API
 function fetchApiData() {
@@ -16,35 +16,44 @@ function fetchApiData() {
     .then(function (data) {
       console.log(data);
       // Added elements onto page
+
       let infoArray = [];
       infoArray = data.infoArray;
         // let { name } = data.infoArray[i];
         let countryFlag = document.createElement('img');
         countryFlag = data[0].flags.png;
         
-
-        // document create element - delete img tag in html 
-        $("#country-flag-div").append(countryFlag);
         // No img replace with something
-        console.log(countryFlag);
-        // if (!image_url)
-        //   image_url =
-        //     "https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg";
+        if (!countryFlag)
+          countryFlag =
+            "https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg";
 
         //Var to add pulled elements to the page
 
         let countryName = document.getElementById('display-country-info');
-        countryName.textcontent = data[0].name.common;
-        // console.log(countryName);
-        // let population = ;
-        // let capital = ;
-        // let continents = ;
-        // let currencies = ;
+        countryName = data[0].name.common;
+
+        let population = document.getElementById('display-country-info');
+        population =  JSON.stringify(data[0].population);
+
+        let capital = document.getElementById('display-country-info');
+        capital = data[0].capital;
+
+        let continents = document.getElementById('display-country-info');
+        continents = data[0].continents;
+
+        let currencies = document.getElementById('display-country-info');
+        currencies = JSON.stringify(data[0].currencies.name);
+
+        var flagBox = `
+        <img src="${countryFlag}" alt="flag">
+        `;
+        $("#country-flag-div").append(flagBox);
 
         var newBox = `
             <div class ="new-country-info">
                 <div class="new-country-text">
-                    <h1>Country: ${countryName}</h1>
+                    <h4>Country: ${userCountry}</h4>
                     <p>Population: ${population} </p>
                     <p>Capital: ${capital}  </p>
                     <p>Continent: ${continents}  </p>
